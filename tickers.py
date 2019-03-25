d5d1afaea0dba24e4ec9ec6c57a6183d5c084a0a
imd15 <imd15@my.fsu.edu>
2019-03-25 19:23:46 -0400

    added try/catch block to check if valid. Write to file tickers.txt

--- tickers.py
+++ tickers.py
@@ -1,26 +1,32 @@
 import sys
 import requests as r
+from iex import Stock
 
 def save_tickers(n):
-    URL = r.get("https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download").text
-
+    #URL = r.get("https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download").text
+    tickerList = []
     pure = r.get("https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=150").text #get pure text from the website
     i =0 #counter
     #print(URL)
-    baseStockUrl = "https://api.iextrading.com/1.0"
+    baseStockUrl = "https://api.iextrading.com/1.0/stock"
     for curr in pure.splitlines(): #splits lines at new line
         if i == n:
             break
         else:
-            subst = curr[curr.rfind("symbol/")::1]
-            ticker = subst[subst.find("/")+1::1]
-            ticker2 = ticker[:ticker.find("/")]
-            if ticker2.find("\"") == -1 and ticker2 != "":
-                print(ticker2)
-                i+=1
-        '''if ticker != "Symbol":
-                print(ticker)
-                i+=1'''
+            try:
+                subst = curr[curr.rfind("symbol/")::1] # parse all lines until 'symbol/' is found https://www.nasdaq.com/symbol/asps/stock-report"> 
+                ticker = subst[subst.find("/")+1::1] # parse subst which contains 'asps/stock-report">'
+                ticker2 = ticker[:ticker.find("/")] #parse to just get the symbol
+                if ticker2.find("\"") == -1 and ticker2 != "":
+                    Stock(ticker2).price() ## checks if valid ticker, if its not found go to except
+                    i = i +1
+                    tickerList.append(ticker2)
+            except:
+                pass
+    
+    file = open("tickers.txt","w") #creates ticker.text to write to it.
+    file.write("\n".join(tickerList)) #puts everything that was in the list created with tickers on a new line
+    file.close() #closes file
 
 
 if __name__ == "__main__":
