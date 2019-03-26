9b518b1d007fefc048f96519d3549d3d3af8f3fc
imd15 <imd15@my.fsu.edu>
2019-03-25 20:36:22 -0400

    created json file to acquire pure text from the url of the qutoe

--- fetcher.py
+++ fetcher.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import json
+import requests as r
 from iex import Stock
 
 def UpdateStockInformation(ticker):
@@ -8,12 +10,15 @@ def UpdateStockInformation(ticker):
 
 
 if __name__ == "__main__":
-    time_limit = sys.argv[1]
-    ticker_filename = sys.argv[2]
-    info_filename = sys.argv[3]
+    #time_limit = sys.argv[1]
+    ticker_filename = "tickers.txt" #sys.argv[2]
+    #info_filename = sys.argv[3]
     ticker_file = open(ticker_filename, "r")
 
     for x in ticker_file:
-        stock = Stock(x)
-        quote = stock.get_quote()
-        print(quote)
+        URL = f"https://ws-api.iextrading.com/1.0/stock/{x.strip()}/quote/"
+        ahhhhh = r.get(URL).text
+        #print(ahhhhh)
+        yeet = json.loads(ahhhhh)
+        print(yeet["symbol"] , yeet["high"])
+        
