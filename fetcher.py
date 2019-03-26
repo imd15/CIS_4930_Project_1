c2032860fb1d28b65f73ece5d608767bc0f2d8e3
imd15 <imd15@my.fsu.edu>
2019-03-25 21:31:47 -0400

    added time so that program runs for certain amount of seconds

--- fetcher.py
+++ fetcher.py
@@ -1,12 +1,13 @@
 import sys
 from time import localtime, strftime
+import time
 import json
 import csv
 import requests as r
 from iex import Stock
 
 def UpdateStockInformation(ticker):
-    URL = f"https://ws-api.iextrading.com/1.0/stock/{x.strip()}/quote/"
+    URL = f"https://ws-api.iextrading.com/1.0/stock/{ticker.strip()}/quote/"
     pureURLtext = r.get(URL).text
     textToJson = json.loads(pureURLtext) #creates dict from what was in url
     Titles = ["symbol","latestPrice", "latestVolume","close","open","low","high"]
@@ -22,17 +23,24 @@ def UpdateStockInformation(ticker):
     
     return
 
-
+def indefiniteUpdate(ticker_file):
+    for x in ticker_file:
+        if time.time() < time_limit:
+            UpdateStockInformation(x)
+        else:
+            break
 
 if __name__ == "__main__":
-    #time_limit = sys.argv[1]
+    time_limit =  time.time() + 10#sys.argv[1]
+
     ticker_filename = "tickers.txt" #sys.argv[2]
     #info_filename = sys.argv[3]
     file = open("info.csv",'w')
-    ticker_file = open(ticker_filename, "r")
     
-    for x in ticker_file:
-        UpdateStockInformation(x)
-        break
-        
+    
+    
+
+    while time.time() < time_limit:
+        ticker_file = open(ticker_filename, "r")
+        indefiniteUpdate(ticker_file)
         
