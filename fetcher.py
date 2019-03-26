8ba5b623c32e5c9c5b8077ea56370a51c839ffa8
imd15 <imd15@my.fsu.edu>
2019-03-25 20:54:29 -0400

    created List containing all necessary things, still need to take care of csv and time

--- fetcher.py
+++ fetcher.py
@@ -1,10 +1,25 @@
 import sys
-import time
+from time import localtime, strftime
 import json
+import csv
 import requests as r
 from iex import Stock
 
 def UpdateStockInformation(ticker):
+    URL = f"https://ws-api.iextrading.com/1.0/stock/{x.strip()}/quote/"
+    ahhhhh = r.get(URL).text
+    yeet = json.loads(ahhhhh)
+    Titles = ["symbol","latestPrice", "latestVolume","close","open","low","high"]
+    timee = strftime("%H:%M")
+    L = [timee]
+    print(Titles)
+    for i in Titles:
+        L.append(yeet[i])
+    
+    print(L)
+    
+    #print(ahhhhh)
+    
     return
 
 
@@ -13,12 +28,11 @@ if __name__ == "__main__":
     #time_limit = sys.argv[1]
     ticker_filename = "tickers.txt" #sys.argv[2]
     #info_filename = sys.argv[3]
+    file = open("info.csv",'w')
     ticker_file = open(ticker_filename, "r")
-
+    
     for x in ticker_file:
-        URL = f"https://ws-api.iextrading.com/1.0/stock/{x.strip()}/quote/"
-        ahhhhh = r.get(URL).text
-        #print(ahhhhh)
-        yeet = json.loads(ahhhhh)
-        print(yeet["symbol"] , yeet["high"])
+        UpdateStockInformation(x)
+        break
+        
         
