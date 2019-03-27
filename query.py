9bede07490cd386427d4b29ba2a3ba257905d4a6
Unknown <kiel.peterson@aol.com>
2019-03-27 01:02:36 -0400

    Almost done with query
    Just needs to take into account sys.args.. Will confirm in office hours that this is the correct approach

--- query.py
+++ query.py
@@ -1,23 +1,19 @@
-import sys
-import csv
+import sys, csv
 
 def Query(info_filename, the_time, ticker):
     with open(info_filename, 'r') as f:
         reader = csv.reader(f)
-        info_list = list(reader)        # creates a list of lists, each list index being a row  
-        info_list = info_list[1:]       # get rid of the column names from the CSV
+        info_list = list(reader)[1:]    # creates a list of lists, each list index being a row
+
         the_tickers = []
         for x in info_list:
-            the_tickers.append(x[1])    # extract just the ticker names from the CSV
+            the_tickers.append(x[1].lower())    # extract just the ticker names from the CSV
+        
+        if ticker not in the_tickers:
+            raise Exception(f"Ticker {ticker} not found in {info_filename}")
 
-        '''
-        if (ticker not in the_tickers):
-            print(f"{ticker} not found in {info_filename}")
-        '''
-    
-    L = []
-
-    return L
+        # return the information of the corresponding ticker at the index in the information list
+        return info_list[the_tickers.index(ticker)]
 
 
 if __name__ == "__main__":
@@ -32,11 +28,24 @@ if __name__ == "__main__":
 
     info_filename = "info.csv"
     the_time = 10
-    ticker = 'aapl'
+    ticker = 'aaon'
 
-    query_information = Query(info_filename, the_time, ticker)
+    # gather the necessary information on the ticker
+    ticker_information = Query(info_filename, the_time, ticker.lower())
+
+    # gather the information on the columns
+    column_names = []
+    with open(info_filename, 'r') as f:
+        reader = csv.reader(f)
+        info_list = list(reader)
+        column_names = info_list[0]
 
     if verbose.lower() == "true":
-        print("verbose")
+        zipped = zip(column_names, ticker_information)
+        for col, info in zipped:
+            print(f"{col}: {info}")
+        # print("verbose")
     else:
-        print("not verbose")
+        pass
+        # print("not verbose")
+    
