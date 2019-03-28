656a978e6fcc553ae46f2a488b95c1237305824c
imd15 <imd15@my.fsu.edu>
2019-03-28 13:52:46 -0400

    prediction testing is slightly working almost done

--- predictor.py
+++ predictor.py
@@ -1,69 +1,52 @@
 import sys,time
 from sklearn.model_selection import train_test_split
-from sklearn import linear_model as lm  
+from sklearn.linear_model import LinearRegression  
 import numpy as np
 import pandas as pd
-import datetime
+from datetime import datetime
 import csv
 import matplotlib.pyplot as plt
-def predictor(fileName, ticker, column, t, graphFile):
-    '''
-    columnVal = []
-    timeVal=[]
-    timeInMinutes = []
-    with open(infoFileName, "r") as infoFile:
-        reader = csv.DictReader(infoFile)
-        for row in reader:
-            if(row['Ticker'] == ticker):
-                columnVal.append(float(row[column]))
-                timeVal.append(datetime.striptime(row["Time"],"%H:%M"))
-
-                
-    print(timeVal)
-    print(columnVal)
-    newTimeVal = []
-
-    for x in timeVal:
-        x = x[:2] + '.' + x[3:]
-        newTimeVal.append(float(x))
-    #print(newTimeVal)
+def predictor(fileName, ticker, column, t, graphFile):    
+    curr = datetime.now()
+    convertToMin=int(curr.hour)*60 + int(curr.minute)
+    timeToPredict= convertToMin + t
     
-    holy = []
+    test = []
+    test.append(timeToPredict)
 
-    for x,y in zip(columnVal,newTimeVal):
-        #print([x, y])
-        holy.append([x,y])
-
-    #print(holy)
-    '''
-    regr = lm.LinearRegression()
-    
     data = pd.read_csv(infoFileName)
     df=data.loc[data['Ticker']==ticker]
     print(df)
     latest = ''+column
 
     X = df[[latest]]
-    Y=df['Time'].str.split(':').apply(lambda a: int(a[0])*60+int(a[1]))
+    Y=df['Time'].str.split(':').apply([lambda a: int(a[0])*60+int(a[1])])
 
     print(X)
     print(Y)
+    xTrain, xTest, yTrain, yTest = train_test_split(Y,X, test_size = 1/3, random_state = 0)
 
-    data.plot(df,y=Y, style = 'o')
-    plt.xlabel('Time in Min')
-    plt.ylabel('Latest PRice')
-    plt.show()  
+    regr = LinearRegression()
+    regr.fit(xTrain,yTrain)
+    yPredict= regr.predict(xTest)
+
+    plt.scatter(xTrain,yTrain,color = 'red')
+    plt.plot(xTrain, regr.predict(xTrain), color = 'blue')
+    plt.xlabel('xxxxxxxxx')
+    plt.ylabel('yyyyyyyyy')
+    #plt.show()
+    plt.savefig(graphFile)
+
+    print(regr.predict([test]))
     #print(X.loc[X['Ticker']==ticker])
     #print(X.values)
 
 
 if __name__ == "__main__":
     ticker = "YI"               #argv[1]
-    infoFileName = "info.csv"   #argv[2]
-    graphFileName = ""          #argv[3]
-    column = "latestPrice"      #argv[4]
+    infoFileName = "info1.csv"   #argv[2]
+    graphFileName = "graph.png"          #argv[3]
+    column = "latestVolume"      #argv[4]
     time = 10                   #argv[5]
     #predictor(fileName,ticker,column, )
     predictor(infoFileName,ticker,column,time,graphFileName)
-    
-    
